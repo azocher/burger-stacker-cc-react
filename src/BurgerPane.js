4b7c1b925ec2dcaddf8bb7033b9babd02fde6611
Anna Zocher <anna.zocher@gmail.com>
2021-06-21 17:06:21 -0700

    az solution from in class

---
+++ src/BurgerPane.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import BurgerStack from './BurgerStack'
+import ClearBurger from './ClearBurger'
+
+
+export default class BurgerPane extends Component {
+    render() {
+        return (
+            <div>
+                <BurgerStack clickedIngredients={this.props.clickedIngredients} />
+                <ClearBurger clearBurger={this.props.clearBurger} />
+            </div>
+        )
+    }
+}
